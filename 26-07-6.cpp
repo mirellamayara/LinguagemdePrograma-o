d03b3fa53e62d4c54afa7f3c533c5ae7b24039e7
mirellamayara <133118078+mirellamayara@users.noreply.github.com>
2023-07-26 09:05:01 -0300

    Add files via upload

---
+++ 26-07-6.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+	int time=20;
+	if(time<18){
+		printf("Bom Dia");
+	}else{
+		printf("Boa Tarde");
+	}
+	return 0;
+	
+}
---
+++ 26-07-8.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int time=20;
+	(time<12)?
+	printf("Bom Dia"):
+	printf("Boa Tarde");
+	return 0;
+}
